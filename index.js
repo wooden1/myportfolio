e5013d265d4acca28d78a0ae154b103177e04335
wooden1 <iblackheartyou@gmail.com>
2018-06-28 15:49:13 -0400

    began seperating nav bar css by media query, various minor tweaks

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // Nav bar text removal for tablet and portrait style
 // :beer:
-function hideElements(){
-    const navText = document.querySelectorAll('.nav-text');
-    const navIcons = document.querySelectorAll('.main-nav > a > i');
-    if (window.innerWidth >= 600){
-       navText.style.display = 'none';
-    }
-    if(window.innerWidth < 600){
-        navIcons.style.visibility = 'hidden';
-    }
-
-}
-hideElements();
+console.log({} + [] === 0);
+// :martial_arts_uniform: :mountain:
