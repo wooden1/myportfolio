57a0321c528b5c3085084e43be141d086ac8e68c
wooden1 <iblackheartyou@gmail.com>
2018-07-01 23:43:53 -0400

    styled desktop view various minor tweaks

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Nav bar text removal for tablet and portrait style
 // :beer:
-console.log({} + [] === 0);
+// console.log({} + [] === 0);
 // :martial_arts_uniform: :mountain:
+
+function something(n, y) {
+    if (n <= y ) {
+        
+        return n / ((y +n) +1);
+    }
+    return ('Please enter valid arguments.');
+}
+console.log(something(200, 320));
