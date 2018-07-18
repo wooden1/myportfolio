821a4a247e2ccbe483d645398200d3fc8ac7cc4a
wooden1 <iblackheartyou@gmail.com>
2018-07-18 14:12:37 -0400

    styled navbar for desktop view, minor various css tweaks

--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 // console.log({} + [] === 0);
 // :martial_arts_uniform: :mountain:
 
-function something(n, y) {
-    if (n <= y ) {
+// function something(n, y) {
+//     if (n <= y ) {
         
-        return n / ((y +n) +1);
-    }
-    return ('Please enter valid arguments.');
-}
-console.log(something(200, 320));
+//         return n / ((y +n) +1);
+//     }
+//     return ('Please enter valid arguments.');
+// }
+// console.log(something(200, 320));
+
+const up = x => x.toUppercase();
+const yea = x =>  x + '!';
+const shout = compose(up, yea);
+
+console.log(shout('send in the clowns'));
